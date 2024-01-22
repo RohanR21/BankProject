bfa406d200404def7c13f50b5523757b8bb6a509
RohanR21 <rohan815r@gmail.com>
2024-01-21 21:20:37 -0500

    Init

---
+++ src/BankAccount.java
@@ -0,0 +1,36 @@
+import java.util.UUID;
+
+public class BankAccount {
+    private final Person person;
+    private double balance;
+    private UUID uuid;
+
+
+    public BankAccount(Person person, double balance) {
+        this.person = person;
+        this.uuid = UUID.randomUUID();
+        if(balance < 0) {
+            this.balance = 0;
+        } else {
+            this.balance = balance;
+        }
+
+        BankAccountManager.BankAccounts.add(this);
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    public UUID getUuid() {
+        return uuid;
+    }
+
+}
---
+++ src/BankAccountManager.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class BankAccountManager {
+    public static ArrayList<BankAccount> BankAccounts = new ArrayList<>();
+    public static BankAccount getBankAccount(UUID uuid) {
+        for(BankAccount account: BankAccounts) {
+            if (account.getUuid() == uuid) {
+                return account;
+            }
+        }
+        return null;
+    }
+    public static BankAccount getBankAccount(Person person) {
+        for(BankAccount account: BankAccounts) {
+            if(account.getPerson() == person) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+
+}
